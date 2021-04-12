5f6732a26b7afa605e8283710301912e0b9555ed
Madison Swain-Bowden <bowdenm@spu.edu>
2021-04-11 18:51:45 -0700

    Change default settings

--- radio_chaser/default_settings.py
+++ radio_chaser/default_settings.py
@@ -1,2 +1,12 @@
-DEBUG = False  # make sure DEBUG is off unless enabled explicitly otherwise
-LOG_DIR = '.'  # create log files in current working directory
+import os
+
+DEBUG = bool(os.environ.get("FLASK_DEBUG", True))
+ENV = os.environ.get("FLASK_ENV", "development")
+# create log files in current working directory
+LOG_DIR = "."
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "SQLALCHEMY_DATABASE_URI",
+    "sqlite:////tmp/radio-chaser.db",
+)
+# To suppress deprecation warnings
+SQLALCHEMY_TRACK_MODIFICATIONS = False
